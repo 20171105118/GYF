62cf5bb181e25997ddbab8eb306666b4b25bb0d7
gyf <s20171105118@mac22.local>
2018-06-21 09:42:30 +0800

    这只是个练习
    这只是个练习

--- GYF/gyf/gyf/main.cpp
+++ GYF/gyf/gyf/main.cpp
@@ -1,14 +1,14 @@
 //
 //  main.cpp
-//  GYF
+//  gyf
 //
 //  Created by gyf on 18/6/21.
 //  Copyright © 2018年 gyf. All rights reserved.
 //
 
 #include <iostream>
-using namespace std;
-int main() {
+
+int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
