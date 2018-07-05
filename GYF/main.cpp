bd5d2c1e65e986205acb40f385d5cc228e89bd17
20171105118 <s20171105118@mac22.local>
2018-07-05 18:25:12 +0800

    添加了输出代码
    读取数据还是最大的难题，无法处理，计算数据也十分头痛，不知道怎么处理。因为读取和计算的问题还未解决，输出是否能成功还未知。下一次将解决之前的问题
    并尝试。

--- GYF/main.cpp
+++ GYF/main.cpp
@@ -82,6 +82,20 @@ int main()
                 break;
         }
     }
+    //定义文件输出流
+    ofstream oFile;
+    //打开要输出的文件
+    oFile.open("Users/s20171105118/Desktop/GYF_work/studentdata.csv", ios::out | ios::trunc);// 这样就很容易的输出一个需要的excel文件
+    oFile << "student number" << "," << "name" << "," << "gender" << "," << "date of birth" << "," << "class" << "," << "telephone" << "judge1" << "," << "judge2" << "," << "judge3" << "," << "judge4" << "," << "judge5" << "," <<endl;
+    oFile << "20171105110" << "," << "Zhangsan" << "," << "man" << "," << "1998.02.03" << "," << "17Network programming" << "," << "1234751923" << "77" << "," << "79" << "," << "85" << "," << "83" << "," << "80" << "," <<endl;
+    oFile << "20171105111" << "," << "Lisi" << "," << "man" << "," << "1998.05.23" << "," << "17Network programming" << "," << "1987041923" << "71" << "," << "76" << "," << "89" << "," << "83" << "," << "83" << "," <<endl;
+    oFile.close();
+    
+    
+    //打开要输出的文件
+    ifstream iFile("scoresheet.csv");
+    string   readStr((std::istreambuf_iterator<char>(iFile)),  std::istreambuf_iterator<char>());
+    cout <<  readStr.c_str();
     
     
     
