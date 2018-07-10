57b9375da4d21e2d8d1b2ca3d5eda1e3fde81e09
20171105118 <s20171105118@mac22.local>
2018-07-10 14:32:33 +0800

    更换失败，换回了原来的代码
    更换失败，换回了自己原来编辑的代码，有错误，求老师帮忙解决

--- GYF/main.cpp
+++ GYF/main.cpp
@@ -8,18 +8,16 @@
 
 #include <fstream>
 #include <sstream>
-#include <string>
 #include <iostream>
 #include <streambuf>
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
 using namespace std;
-
 int main()
 {
-
-    FILE *a,*b;
+    
+    FILE *a;
     string line;
     char name[50][100],clas[50][100],gender[50][100],db[50][100],phone[50][100],number[50][100];
     int judge[5];
@@ -28,9 +26,9 @@ int main()
     int p;
     ifstream fin("/Users/s20171105118/Desktop/GYF/studentdata.csv");
     while (getline(fin, line))
-        {
-            ++l;
-        }
+    {
+        ++l;
+    }
     if((a=fopen("/Users/s20171105118/Desktop/GYF/studentdata.csv","r"))==0)
     {
         printf("文件不存在\n");
@@ -44,11 +42,11 @@ int main()
                     fscanf(a,"%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d",number[i],name[i],gender[i],db[i],clas[i],phone[i],&judge[0],&judge[1],&judge[2],&judge[3],&judge[4]);
                     
                 }
-                
-                int max=judge[0];
-                int min=judge[0];
-                for(p=0; p<5; p++)
-                {
+            int max=judge[0];
+            int min=judge[0];
+            double pjz[i];
+                    for(p=0; p<5; p++)
+                    {
                     if(judge[p]>max)
                     {
                         max=judge[p];
@@ -63,23 +61,14 @@ int main()
             }
             if(i==l)
                 break;
-        }
+            }
         
         
     }
-    fclose(a);
-   b=fopen("/Users/s20171105118/Desktop/GYF/studentout.csv","w");
-    for(i=0;i<l;i++){
-    fprintf(b,"%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d",number[i],name[i],gender[i],db[i],clas[i],phone[i],judge[0],judge[1],judge[2],judge[3],judge[4]);
-    }
-    fclose(b);
-  /*  ofstream oFile("Users/s20171105118/Desktop/GYF/studentdataout.csv");
+    ofstream oFile("Users/s20171105118/Desktop/GYF/studentdataout.csv");
     oFile << "numbers,name,sex,dateofbirth,class,phoneNO,average\n";
     for(i=0;i<l;++i){
-        oFile<< number[i] << ","<< name[i] << ","<< gender[i] << ","<< db[i] << ","<< clas[i] << ","<< phone[i] << ","<< pjz[i]<<endl;
-
+        oFile<< number[i] << ","<< name[i] << ","<< gender[i] << ","<< db[i] << ","<< clas[i] << ","<< phone[i] << ","<< pjz[i]<<std::endl;
     }
-*/
-
     return 0;
     }
